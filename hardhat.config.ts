4ad78ef61a83e74a90b9dd3fdca930d7438d78a2
Jeffrey <jeffreyhu16@gmail.com>
2022-08-21 10:12:32 +0800

    remove redundant dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,10 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@typechain/hardhat"
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-etherscan"
-import "@nomiclabs/hardhat-ethers"
-import "hardhat-gas-reporter"
-import "dotenv/config"
-import "solidity-coverage"
-import "hardhat-deploy"
-import "solidity-coverage"
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
 };
 
 export default config;
