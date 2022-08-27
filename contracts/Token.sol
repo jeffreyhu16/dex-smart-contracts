1a9af1a240d728264f54a1bec7da1df0a354f619
Jeffrey <jeffreyhu16@gmail.com>
2022-08-27 10:49:49 +0800

    add transfer function to Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,26 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "Nexus Plus";
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    mapping(address => uint256) public tokenBalances;
 
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * 1e18;
+        tokenBalances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        tokenBalances[msg.sender] -= _value;
+        tokenBalances[_to] += _value;
+        (bool success,) = _to.call{value: _value}("");
+        return success;
+    }
 }
