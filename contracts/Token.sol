ce3e58c3d2ecfde4f2c6636c294346a7ac5d5846
Jeffrey <jeffreyhu16@gmail.com>
2022-08-27 18:20:55 +0800

    add approve function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,16 +4,24 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 error Token__InsufficientFunds();
+error Token__ApprovingZeroAddress();
 
 contract Token {
     string public name;
     string public symbol;
     uint256 public totalSupply;
     mapping(address => uint256) public tokenBalances;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed from,
         address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
 
@@ -28,7 +36,7 @@ contract Token {
         tokenBalances[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         if (tokenBalances[msg.sender] < _value) {
             revert Token__InsufficientFunds();
         }
@@ -36,6 +44,15 @@ contract Token {
         tokenBalances[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        if (_spender == address(0)) {
+            revert Token__ApprovingZeroAddress();
+        }
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 }
