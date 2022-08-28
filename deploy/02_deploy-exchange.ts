15348f32f8865679ffb9aea5ecebe41ce7ef53d3
Jeffrey <jeffreyhu16@gmail.com>
2022-08-28 18:02:12 +0800

    add deploy script for Exchange.sol

---
+++ deploy/02_deploy-exchange.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployExchange: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments: { deploy, log }, getNamedAccounts } = hre;
+    const { deployer } = await getNamedAccounts();
+
+    const exchange = await deploy('Exchange', {
+        from: deployer,
+        log: true,
+        args: [deployer, ethers.utils.parseEther('0.01')]
+    });
+}
+
+export default deployExchange;
+deployExchange.tags = ['all', 'exchange'];
