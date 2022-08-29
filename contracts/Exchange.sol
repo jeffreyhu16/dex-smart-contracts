2289ff7490bb8fb41e340a062a04d230db7ce838
Jeffrey <jeffreyhu16@gmail.com>
2022-08-29 14:50:01 +0800

    add makeOrder function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -3,16 +3,47 @@ pragma solidity ^0.8.9;
 
 import "./Token.sol";
 
+error Exchange__InsufficientDeposit();
+
 contract Exchange {
+    struct Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
+
     address public feeAccount;
     uint256 public feeRate;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => Order) public orders;
+    uint256 public orderCount;
 
     event Deposit(
         address token, 
         address user, 
         uint256 amount, 
         uint256 balance
+    );
+
+    event Withdraw(
+        address token, 
+        address user, 
+        uint256 amount, 
+        uint256 balance
+    );
+
+    event OrderMade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
     );
 
     constructor(address _feeAccount, uint256 _feeRate) {
@@ -24,5 +55,41 @@ contract Exchange {
         Token(_token).transferFrom(msg.sender, address(this), _amount);
         tokens[_token][msg.sender] += _amount;
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        Token(_token).transfer(msg.sender, _amount);
+        tokens[_token][msg.sender] -= _amount;
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        if (tokens[_tokenGive][msg.sender] < _amountGive) {
+            revert Exchange__InsufficientDeposit();
+        }
+        orderCount += 1;
+        orders[orderCount] = Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+        emit OrderMade(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
     }
 }
