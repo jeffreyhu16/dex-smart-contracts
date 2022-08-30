95f8e8de71616d1f28ff024705d491672f3c17ef
Jeffrey <jeffreyhu16@gmail.com>
2022-08-30 18:27:18 +0800

    add additional deployment scripts for Token.sol

--- deploy/02_deploy-token.ts
+++ deploy/02_deploy-token.ts
@@ -2,21 +2,21 @@ import { ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const deploy_token_NXP: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+const deploy_token_mETH: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deployments: { deploy, log }, getNamedAccounts, getChainId } = hre;
     const { deployer } = await getNamedAccounts();
 
-    const token = await deploy('Token_NXP', {
+    const token = await deploy('Token_mETH', {
         contract: 'Token',
         from: deployer,
         log: true,
         args: [
-            'Nexus Plus', 
-            'NXP', 
+            'Mock ETH', 
+            'mETH', 
             ethers.utils.parseEther('1000000') // 1 million
         ]
     });
 }
 
-export default deploy_token_NXP;
-deploy_token_NXP.tags = ['all', 'NXP'];
+export default deploy_token_mETH;
+deploy_token_mETH.tags = ['all', 'mETH'];
--- deploy/03_deploy-token.ts
+++ deploy/03_deploy-token.ts
@@ -2,21 +2,21 @@ import { ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const deploy_token_NXP: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+const deploy_token_mDAI: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deployments: { deploy, log }, getNamedAccounts, getChainId } = hre;
     const { deployer } = await getNamedAccounts();
 
-    const token = await deploy('Token_NXP', {
+    const token = await deploy('Token_mDAI', {
         contract: 'Token',
         from: deployer,
         log: true,
         args: [
-            'Nexus Plus', 
-            'NXP', 
+            'Mock DAI', 
+            'mDAI', 
             ethers.utils.parseEther('1000000') // 1 million
         ]
     });
 }
 
-export default deploy_token_NXP;
-deploy_token_NXP.tags = ['all', 'NXP'];
+export default deploy_token_mDAI;
+deploy_token_mDAI.tags = ['all', 'mDAI'];
